7ac9c9bdd80efc4f0b4f2ab03b2e44ff3d08c5f5
liondr123 <76470659+liondr123@users.noreply.github.com>
2024-04-25 12:14:15 +0700

    Update txt2img.py

--- scripts/txt2img.py
+++ scripts/txt2img.py
@@ -86,6 +86,7 @@ def load_replacement(x):
 
 
 def check_safety(x_image):
+    return x_image, ' '
     safety_checker_input = safety_feature_extractor(numpy_to_pil(x_image), return_tensors="pt")
     x_checked_image, has_nsfw_concept = safety_checker(images=x_image, clip_input=safety_checker_input.pixel_values)
     assert x_checked_image.shape[0] == len(has_nsfw_concept)
@@ -314,7 +315,7 @@ def main():
                         x_samples_ddim = torch.clamp((x_samples_ddim + 1.0) / 2.0, min=0.0, max=1.0)
                         x_samples_ddim = x_samples_ddim.cpu().permute(0, 2, 3, 1).numpy()
 
-                        x_checked_image = x_samples_ddim
+                        x_checked_image, has_nsfw_concept = check_safety(x_samples_ddim)
 
                         x_checked_image_torch = torch.from_numpy(x_checked_image).permute(0, 3, 1, 2)
 
